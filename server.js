303ef7938b4f7d54b01a94d1d13ec078f347f7c9
erickgarciabrq <46604818+erickgarciabrq@users.noreply.github.com>
2019-01-15 11:44:57 -0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/',(req, res)=> {
     res.status(200).send({ message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/Olá',(req, res)=> {
+    res.status(200).send({ message: 'Oi eu sou a rota olá !'});
+})
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
